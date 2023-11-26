769290fa105603f663096eaf626795ed5ceb151c
expert26111 <djoana.v@gmail.com>
2023-11-26 01:13:09 +0100

    Commiting code for Majority Element Easy

---
+++ MajorityElement/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// CHECK LINK TO IMPROVE PROGRAMME
+// https://tutorialcup.com/leetcode-solutions/majority-element-leetcode-solution.htm
+
+using MajorityElement;
+using System;
+
+Console.WriteLine("Hello, World!");
+//int[] nums = { 2, 2, 1, 1, 1, 2, 2 };
+//int[] nums = { 3, 3, 4 };
+//int[] nums = { 3, 3, 4 };
+int[] nums = { 3, 2, 3 };
+Solution sol = new Solution();
+int solution = sol.MajorityElement(nums);
+Console.WriteLine("The majority element is: {0}", solution);
---
+++ MajorityElement/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MajorityElement
+{
+    internal class Solution
+    {
+        public int MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            // SortedDictionary<int, int> dict = new SortedDictionary<int, int>(Comparer<int>.Create((x, y) => x.CompareTo(y)));
+            // SortedDictionary<int, int> dict = new SortedDictionary<int, int>().OrderBy();
+            int lenghtDict = nums.Length / 2;
+            foreach (int num in nums)
+            {
+                if (dict.ContainsKey(num))
+                {
+                    dict[num] = dict[num]+1;
+                }
+                else
+                {
+                    dict[num] = 1;
+                }
+
+            }
+
+            //   int thenum = dict.Keys.ElementAt(0);
+            var topMajorityNumber = dict.OrderByDescending(pair => pair.Value).Take(1).ToList();
+                
+            /*
+            if (dict[thenum]> lenghtDict)
+            {
+                return thenum;
+            }
+            else
+            {
+                return -1;
+            }
+            */
+         //   return -1;
+            return topMajorityNumber[0].Key;
+          
+
+        }
+    }
+}
